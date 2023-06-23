9442fac13932581aef7c4b2e15373013af952079
Viktor2408 <89632930+Viktor2408@users.noreply.github.com>
2023-06-23 14:10:21 +0300

    Add files via upload

---
+++ tasks/task 2/main.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#gosNumber").mask("a999aa 999");
+    $("#arrivalDate").mask("99.99.9999");
+    $("#series").mask("99 99");
+    $("#number").mask("999 999");
+    $("#whenIssued").mask("99.99.9999");
+  });
