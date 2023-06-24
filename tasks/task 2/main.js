3b5b6c0cdf2774d132c9f76d990ce8c09937b895
Viktor2408 <89632930+Viktor2408@users.noreply.github.com>
2023-06-24 10:47:00 +0300

    Update main.js

--- tasks/task 2/main.js
+++ tasks/task 2/main.js
@@ -1,6 +1,5 @@
 $(function() {
     $("#gosNumber").mask("a999aa 999");
-    $("#arrivalDate").mask("99.99.9999");
     $("#series").mask("99 99");
     $("#number").mask("999 999");
     $("#whenIssued").mask("99.99.9999");
